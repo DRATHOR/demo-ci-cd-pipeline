d0d39496d61f2d0cded1481254047776ded57e99
Devendra Kumar Rathore <devendra143143@gmail.com>
2025-11-17 15:12:00 +0530

    Revert "Initialize project using Create React App"
    This reverts commit 7cbe418628f36c6bfd9c2a14d84c251592f475d5.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return <div className="App">Welcome to CI/CD Pipe line setup app</div>;
+  return (
+    <div className="App">
+      <header>Git Hub Course</header>
+    </div>
+  );
 }
 
 export default App;
