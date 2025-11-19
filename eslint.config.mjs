4ac9094b7108cdd6a679a98b12b02c15b6c18390
Devendra Kumar Rathore <devendra143143@gmail.com>
2025-11-19 13:02:03 +0530

    change config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,27 @@
-import js from "@eslint/js";
-import globals from "globals";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
-]);
+export default [
+  js.configs.recommended,
+
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+      },
+    },
+    plugins: {
+      react,
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off', // React 17+
+      'react/prop-types': 'off',
+    },
+  },
+
+  react.configs.flat.recommended,
+];
